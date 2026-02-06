780462d3e7cd687105c96bf79a89625c0aff360e
ewoillar <ewoillar@cybersecurity.myguest.virtualbox.org>
2026-02-06 18:21:34 +0100

    add docker-compose to test

---
+++ Inquisitor/container1/inquisitor.py
@@ -0,0 +1,48 @@
+import argparse
+import ipaddress
+import re
+from scapy.all import sniff, ARP
+
+def check_ipv4(ip:str) -> bool:
+    try:
+        ip_object = ipaddress.ip_address(ip)
+        return True
+    except ValueError:
+        return False
+
+def check_mac(mac: str) -> bool:
+    patterns = [
+        r'^([0-9A-Fa-f]{2}-){5}[0-9A-Fa-f]{2}$',   # xx-xx-xx-xx-xx-xx
+        r'^([0-9A-Fa-f]{2}:){5}[0-9A-Fa-f]{2}$',   # xx:xx:xx:xx:xx:xx
+        r'^([0-9A-Fa-f]{3}\.){3}[0-9A-Fa-f]{3}$',  # xxx.xxx.xxx.xxx
+    ]
+    return any(re.fullmatch(p, mac) for p in patterns)
+
+def handle(pkt):
+    if not pkt.haslayer(ARP):
+        return
+    arp = pkt[ARP]
+
+    if (
+        arp.psrc == src[0] and
+        arp.hwsrc.lower() == src[1] and
+        arp.pdst == target[0] and
+        arp.hwdst.lower() == target[1]
+    ):
+        print(f"[ARP] {arp.psrc} ({arp.hwsrc}) → {arp.pdst} ({arp.hwdst})")
+#--------------------argparse--------------------
+parser = argparse.ArgumentParser(description="educative program about ARP poisoning")
+parser.add_argument("IP-src", help="ip v4 address of the source")
+parser.add_argument("MAC-src", help="MAC address of the source")
+parser.add_argument("IP-target", help="ip v4 address of the target")
+parser.add_argument("MAC-target", help="MAC address of the target")
+args = parser.parse_args()
+#--------------------checker--------------------
+src = (getattr(args, "IP-src").strip(), getattr(args, "MAC-src").strip().lower())
+target = (getattr(args, "IP-target").strip(), getattr(args, "MAC-target").strip().lower())
+
+if not check_ipv4(src[0]) or not check_ipv4(target[0]) or not check_mac(src[1]) or not check_mac(target[1]):
+    print("Wrong format for IP or MAC address")
+    exit(1)
+
+sniff(filter="arp", prn=handle, store=False)
